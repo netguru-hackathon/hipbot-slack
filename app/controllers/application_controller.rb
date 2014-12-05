9516c840b36a86bf594779cc8c99e9d20ec2cf2b
Tomasz Pewiński <pewniak747@gmail.com>
2014-12-05 12:41:12 +0100

    comment echo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   def slack
     Rails.logger.info(params[:text])
-    SlackConnector.message(nil, "Got it, #{params[:text]}")
+    # SlackConnector.message(nil, "Got it, #{params[:text]}")
     render nothing: true
   end
 end
