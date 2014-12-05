cebadb64592e873cb82001b792137f2dd43da6f4
Tomasz Pewiński <pewniak747@gmail.com>
2014-12-05 12:52:04 +0100

    even more verbose logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   def slack
     Rails.logger.info(params)
-    Rails.logger.info(params[:user_name], params[:user_id])
-    if params[:user_name] !~ /bot/i
+    Rails.logger.info(params[:user_name])
+    Rails.logger.info(params[:user_id])
+    if params[:user_name] !~ /bot/
+      Rails.logger.info('not a bot')
       # SlackConnector.message(nil, "Got it, #{params[:text]}")
     end
     render nothing: true
