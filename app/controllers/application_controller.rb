6832dc533c7010ca9f27a6b3b9bf33a1b1e102a2
Tomasz Pewiński <pewniak747@gmail.com>
2014-12-05 12:46:31 +0100

    prevent loops, try #2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   def slack
     Rails.logger.info(params)
-    if params[:user_name] !~ /hipbot/i
+    if params[:user_name] !~ /bot/i
       SlackConnector.message(nil, "Got it, #{params[:text]}")
     end
     render nothing: true
