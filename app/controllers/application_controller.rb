ad2758f5d2af6e72a8f1d41ddb9bb4d2b0ff9879
Tomasz Pewiński <pewniak747@gmail.com>
2014-12-05 12:53:50 +0100

    activate echo again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
     Rails.logger.info(params[:user_name])
     Rails.logger.info(params[:user_id])
     if params[:user_name] !~ /bot/
-      Rails.logger.info('not a bot')
-      # SlackConnector.message(nil, "Got it, #{params[:text]}")
+      SlackConnector.message(nil, "Got it, #{params[:text]}")
     end
     render nothing: true
   end
